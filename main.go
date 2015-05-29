32f8167843acd689d964c3b80b0df1b7af5325ac
Liu Yang <liu_yang@worksap.co.jp>
2015-05-29 21:11:46 +0800

    Add file progress sse.

--- main.go
+++ main.go
@@ -30,13 +30,11 @@ type FileResource struct {
 func (f FileResource) Register(container *restful.Container) {
 	ws := new(restful.WebService)
 	ws.
-		Path("/files").
-		Consumes(restful.MIME_XML, restful.MIME_JSON).
-		Produces(restful.MIME_JSON, restful.MIME_XML)
+		Path("/files").Produces("*/*")
 
-	ws.Route(ws.GET("/{id}").To(f.getFileInfo))
+	ws.Route(ws.GET("/{id}").To(f.getFileInfo).Produces("text/event-stream"))
 	ws.Route(ws.GET("/{id}/download").To(f.downloadFile))
-	ws.Route(ws.POST("").To(f.createFile))
+	ws.Route(ws.POST("").To(f.createFile).Consumes(restful.MIME_JSON))
 	ws.Route(ws.PUT("/{id}").To(f.uploadFile).Consumes("multipart/form-data"))
 
 	container.Add(ws)
@@ -74,6 +72,7 @@ func (f FileResource) downloadFile(request *restful.Request, response *restful.R
 	http.ServeFile(response, request.Request, path)
 }
 func (f FileResource) getFileInfo(request *restful.Request, response *restful.Response) {
+	log.Println("test")
 	file, err := f.findFile(request.PathParameter("id"))
 	if file == nil {
 		response.AddHeader("Content-Type", "text/plain")
@@ -85,7 +84,50 @@ func (f FileResource) getFileInfo(request *restful.Request, response *restful.Re
 		response.WriteErrorString(http.StatusInternalServerError, err.Error())
 		return
 	}
-	response.WriteEntity(file)
+	w := response.ResponseWriter
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		response.AddHeader("Content-Type", "text/plain")
+		response.WriteErrorString(http.StatusInternalServerError, "Streaming unsupported!")
+		return
+	}
+	ticker := time.NewTicker(time.Second)
+	notify := w.(http.CloseNotifier).CloseNotify()
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	fmt.Fprintf(w, "data: {\"type\": \"name\", \"content\": \"%s\"}\n\n", file.Name)
+	flusher.Flush()
+	if file.Status == "uploading" || file.Status == "init" {
+		for _ = range ticker.C {
+    		   fileInfo, err := f.findFile(request.PathParameter("id"))
+			
+			if fileInfo == nil {
+				fmt.Fprintf(w, "data: {\"type\": \"error\", \"content\": \"not found\"}\n\n")
+				flusher.Flush()
+			}
+			if err != nil {
+				fmt.Fprintf(w, "data: {\"type\": \"error\", \"content\": \"%s\"}\n\n", err.Error())
+				flusher.Flush()
+			}
+		    if fileInfo.Status == "uploading" || file.Status == "init" {
+				fmt.Fprintf(w, "data: {\"type\": \"progress\", \"content\": \"%f\"}\n\n", fileInfo.Progress)
+				flusher.Flush()
+			}
+			if fileInfo.Status == "uploaded" || fileInfo.Status == "failed" {
+				fmt.Fprintf(w, "data: {\"type\": \"done\", \"content\": \"%s\"}\n\n", fileInfo.Status)
+				flusher.Flush()
+				ticker.Stop()
+			}
+        }
+	} else {
+		fmt.Printf("writing")
+		fmt.Fprintf(w, "data: {\"type\": \"done\", \"content\": \"%s\"}\n\n", file.Status)
+		flusher.Flush()
+	}
+
+	<-notify
+	ticker.Stop()
 }
 
 func (f *FileResource) createFile(request *restful.Request, response *restful.Response) {
@@ -159,6 +201,7 @@ func (f *FileResource) uploadFile(request *restful.Request, response *restful.Re
 		}
 	}
 	fileInfo.Status = "uploading"
+	fileInfo.Progress = 0
 	saveFile()
 
 	path := filepath.Join(f.dir, fileInfo.Id)
